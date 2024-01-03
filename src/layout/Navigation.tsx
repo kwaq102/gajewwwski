fe5fc80ccbab1087d7762ad0b4b9affc28e68f09
Kamil Gajewski <kwaq102@gmail.com>
2024-01-03 13:21:25 +0100

    add navigation without logo and small changes in header

--- src/layout/Navigation.tsx
+++ src/layout/Navigation.tsx
@@ -1,28 +1,36 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { navList } from "../ts/navigationList";
 
 const Navigation = () => {
 	const [menuOpen, setMenuOpen] = useState(false);
+	const [bgNav, setBgNav] = useState(false);
+
+	const navigation = navList.map(el => (
+		<li key={el.navName} className="main-nav__item">
+			<NavLink to={el.path} end={el.end} className="main-nav__link">
+				{el.navName}
+			</NavLink>
+		</li>
+	));
+
+	const handleScroll = () => {
+		if (window.scrollY > 60) {
+			setBgNav(true);
+		} else {
+			setBgNav(false);
+		}
+	};
+
+	window.addEventListener("scroll", handleScroll);
 
 	return (
-		<nav className="main-nav">
-			<div className="main-nav__logo"></div>
-			<ul className="main-nav__list">
-				<li className="main-nav__item">
-					<NavLink className="main-nav__link" to="/oferta">
-						Oferta
-					</NavLink>
-				</li>
-				<li className="main-nav__item">
-					<NavLink className="main-nav__link" to="/cos">
-						Coś
-					</NavLink>
-				</li>
-				<li className="main-nav__item">
-					<NavLink className="main-nav__link" to="/test">
-						Test
-					</NavLink>
-				</li>
+		<nav className={`main-nav ${bgNav && "nav-bg"}`}>
+			<div className="main-nav__logo">
+				gajewwwski<span>.pl</span>
+			</div>
+			<ul className={`main-nav__list ${menuOpen && "show-nav"}`}>
+				{navigation}
 			</ul>
 			<div
 				className={`main-nav__btn ${menuOpen && "open"}`}
---
+++ src/ts/navigationList.ts
@@ -0,0 +1,7 @@
+export const navList = [
+    { navName: "Strona Główna", path: "/", end: true },
+    { navName: "O mnie", path: "/o-mnie" },
+    { navName: "Oferta", path: "/oferta" },
+    { navName: "Portfolio", path: "/portfolio" },
+    { navName: "Kontakt", path: "/kontakt" },
+];
