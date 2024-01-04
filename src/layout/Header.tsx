66b2c7192fa36645a95b863e875d7208fc30203f
Kamil Gajewski <kwaq102@gmail.com>
2024-01-04 23:44:32 +0100

    rwd for header and navigation

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -13,48 +13,47 @@ const Header = () => {
 	const textToTyping = useTypingEffect(text, 50);
 
 	return (
-		<header className="header">
-			<div className="header-box">
-				<div className="box-left">
-					{/* TODO pomyśleć nad zdjęciem */}
-					{/* <img src={aiWoman} alt="sztuczna inteligencja kobieta" /> */}
-				</div>
-				<div className="box-right">
-					<h1 className="header-title">
-						Strony internetowe <strong>gajewwwski.pl</strong>
-					</h1>
-					{/* TODO tekt do zmiany */}
-					<p className="header-text">
-						You can easily change any design to your own. It is also highly
-						customizable SEO friendly template.
-					</p>
-					<p className="header-text header-typing">{textToTyping}</p>
+		<>
+			<header className="header">
+				<div className="box-left"></div>
 
-					{/* TODO dodać adresy */}
-					<div className="socials">
-						<a href="#">
-							<span className="fab fa-facebook-f"></span>
-						</a>
-						<a href="#">
-							<span className="fab fa-linkedin-in"></span>
-						</a>
-						<a href="#">
-							<span className="fab fa-github"></span>
-						</a>
-						<a href="#">
-							<span className="fa-solid fa-link"></span>
-						</a>
+				<div className="header-box">
+					<div className="box-content">
+						<h1 className="header-title">
+							Strony internetowe <strong>gajewwwski.pl</strong>
+						</h1>
+						{/* TODO tekt do zmiany */}
+						<p className="header-text">
+							You can easily change any design to your own. It is also highly
+							customizable SEO friendly template.
+						</p>
+						<p className="header-text header-typing">{textToTyping}</p>
+
+						{/* TODO dodać adresy */}
+						<div className="socials">
+							<a href="#">
+								<span className="fab fa-facebook-f"></span>
+							</a>
+							<a href="#">
+								<span className="fab fa-linkedin-in"></span>
+							</a>
+							<a href="#">
+								<span className="fab fa-github"></span>
+							</a>
+							<a href="#">
+								<span className="fa-solid fa-link"></span>
+							</a>
+						</div>
+
+						<div className="box-buttons">
+							<ButtonLink textBtn="Skontaktuj się" href="#" />
+							<ButtonLink textBtn="Dowiedz się więcej" href="#" />
+						</div>
 					</div>
 				</div>
-
-				<div className="box-buttons">
-					<ButtonLink textBtn="Skontaktuj się" href="#" />
-					<ButtonLink textBtn="Dowiedz się więcej" href="#" />
-				</div>
-			</div>
-
+			</header>
 			<Particles />
-		</header>
+		</>
 	);
 };
 
