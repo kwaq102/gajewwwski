a5312b29771eec0fb4e69a898294577bd952b153
Kamil Gajewski <kwaq102@gmail.com>
2024-01-22 23:28:13 +0100

    added styles to motto section

--- src/layout/Motto.tsx
+++ src/layout/Motto.tsx
@@ -1,9 +1,13 @@
 const Motto = () => {
 	return (
-		<p className="motto">
-			Sukces to świadomość, że twoje dzieło ma znaczenie i pozytywnie wpływa na
-			innych. <span className="motto__author">Helen Keller</span>
-		</p>
+		<section className="motto">
+			<div className="motto__box">
+				<p className="motto__text">
+					Sukces to świadomość, że twoje dzieło ma znaczenie i pozytywnie wpływa
+					na innych. <span className="motto__author">Helen Keller</span>
+				</p>
+			</div>
+		</section>
 	);
 };
 
