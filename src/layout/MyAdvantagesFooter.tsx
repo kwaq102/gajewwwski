2950c802548dde305f9b79c4018e10e6201543b1
Kamil Gajewski <kwaq102@gmail.com>
2025-04-01 21:56:25 +0200

    changed names of variables in project

--- src/layout/MyAdvantagesFooter.tsx
+++ src/layout/MyAdvantagesFooter.tsx
@@ -1,14 +1,18 @@
 const MyAdvantagesFooter = () => {
 	return (
 		<section className="myAdvantages__footer">
-			<h3>Współpraca oparta na wartościach</h3>
+			<h3 className="myAdvantages__footer__heading ">
+				Współpraca oparta na wartościach
+			</h3>
 			<p className="myAdvantages__footer-text">
 				Wierzę, że te wartości sprawiają, że jestem odpowiednim partnerem do
 				realizacji Twoich celów biznesowych. Jeśli cenisz profesjonalizm, pasję
 				i zaangażowanie, a współpraca oparta na zaufaniu i dobrej atmosferze
 				jest dla Ciebie ważna – zapraszam do współpracy!
 			</p>
-			<button>Zacznij współpracę</button>
+			<button className="btn myAdvantages__footer__btn">
+				Zacznij współpracę
+			</button>
 		</section>
 	);
 };
